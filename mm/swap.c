b3a48a746d2ed51101dae1684c9f1ded26293d68
Yu Zhao <yuzhao@google.com>
2022-06-14 22:47:20 -0600

    mglru fixes
    Change-Id: I09999793fd6481bcf2acefb29c57d5d40c0b1427
    Signed-off-by: Yu Zhao <yuzhao@google.com>

--- mm/swap.c
+++ mm/swap.c
@@ -443,7 +443,7 @@ static void __lru_cache_add(struct page *page)
 	struct pagevec *pvec = &get_cpu_var(lru_add_pvec);
 
 	/* see the comment in lru_gen_add_page() */
-	if (lru_gen_enabled() && !PageUnevictable(page) && !PageActive(page) &&
+	if (lru_gen_enabled() && !PageUnevictable(page) &&
 	    lru_gen_in_fault() && !(current->flags & PF_MEMALLOC))
 		SetPageActive(page);
 
@@ -588,7 +588,7 @@ static void lru_deactivate_file_fn(struct page *page, struct lruvec *lruvec,
 static void lru_deactivate_fn(struct page *page, struct lruvec *lruvec,
 			    void *arg)
 {
-	if (PageLRU(page) && PageActive(page) && !PageUnevictable(page)) {
+	if (PageLRU(page) && !PageUnevictable(page) && (PageActive(page) || lru_gen_enabled())) {
 		int file = page_is_file_cache(page);
 
 		del_page_from_lru_list(page, lruvec);
@@ -696,7 +696,7 @@ void deactivate_file_page(struct page *page)
  */
 void deactivate_page(struct page *page)
 {
-	if (PageLRU(page) && PageActive(page) && !PageUnevictable(page)) {
+	if (PageLRU(page) && !PageUnevictable(page) && (PageActive(page) || lru_gen_enabled())) {
 		struct pagevec *pvec = &get_cpu_var(lru_deactivate_pvecs);
 
 		get_page(page);
